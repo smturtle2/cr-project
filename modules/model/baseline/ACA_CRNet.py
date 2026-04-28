070eae8620ee40125dd63eb1ad1b31b992c5e101
dongju <smturtle2@kakao.com>
2026-04-28 21:41:15 +0900

    feat: add ACA_CRNet and ConAttn modules with initialization functions

---
+++ modules/model/baseline/ACA_CRNet.py
@@ -0,0 +1,114 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on May 9 10:24:49 2024
+
+@author: Wenli Huang
+"""
+
+import torch.nn as nn
+from collections import OrderedDict
+from torch.nn import init
+import torch
+from .ca import ConAttn
+
+#resnet block
+class ResBlock(nn.Module):
+    def __init__(self,in_channels,out_channels=256,alpha=0.1):
+        super(ResBlock, self).__init__()
+        m = OrderedDict()
+        m['conv1']=nn.Conv2d(in_channels, out_channels,kernel_size=3,bias=False,stride=1,padding=1)
+        m['relu1']=nn.ReLU(True)
+        m['conv2']=nn.Conv2d(out_channels,out_channels, kernel_size=3,bias=False,stride=1,padding=1)
+        self.net = nn.Sequential(m)
+        self.relu= nn.Sequential(nn.ReLU(True))
+        self.alpha = alpha
+    #out = 256+2*0-1*(3-1)-1 +1 = 256
+    def forward(self,x):
+        out =self.net(x)
+        out = self.alpha*out + x
+        return out
+
+#resnet模块
+class ResBlock_att(nn.Module):
+    def __init__(self,in_channels,out_channels=256,alpha=0.1):
+        super(ResBlock_att, self).__init__()
+        m = OrderedDict()
+        m['conv1']=nn.Conv2d(in_channels, out_channels,kernel_size=3,bias=False,stride=2,padding=1)
+        m['relu1']=nn.ReLU(True)
+        m['conv2']=nn.Conv2d(out_channels,out_channels, kernel_size=3,bias=False,stride=1,padding=1)
+        m['relu2'] = nn.ReLU(True)
+        m['att'] = ConAttn(input_channels=out_channels, output_channels=out_channels, ksize=1, stride=1)
+        self.net = nn.Sequential(m)
+        #self.relu= nn.Sequential(nn.ReLU(True))
+        self.alpha = alpha
+    #out = 256+2*0-1*(3-1)-1 +1 = 256
+    def forward(self,x):
+        out =self.net(x)
+        out = torch.nn.functional.interpolate(out, scale_factor=2, mode='bilinear',
+                                            align_corners=True)
+        out = self.alpha*out + x
+        return out
+
+def init_net(net, init_type="kaiming-uniform", gpu_ids=[]):
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        net.to(gpu_ids[0])
+        net = torch.nn.DataParallel(net, gpu_ids)
+    init_weights(net, init_type)
+    return net
+
+# see section 6.2.1
+def init_weights(net, init_type="kaiming-uniform", gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == "kaiming-uniform":
+                init.kaiming_uniform_(m.weight.data, mode='fan_in', nonlinearity='relu')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal_(m.weight.data, 1.0, gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)
+
+class ACA_CRNet(nn.Module):
+    def __init__(self,in_channels,out_channels,alpha=0.1,num_layers = 16 , feature_sizes = 256,gpu_ids=[]):
+        super(ACA_CRNet,self).__init__()
+        m= []
+        m.append(nn.Conv2d(in_channels,out_channels=feature_sizes,kernel_size=3,bias=True,stride = 1 ,padding=1))
+        m.append(nn.ReLU(True))
+        for i in range(num_layers):
+
+            #if i == 6:
+            #    m.append(ConAttn(input_channels=feature_sizes, output_channels=feature_sizes, ksize=1, stride=1))
+
+            if i == num_layers//2:
+                m.append(ResBlock_att(feature_sizes,feature_sizes,alpha))# 256/s==int
+            elif i == num_layers*3//4:
+                m.append(ResBlock_att(feature_sizes,feature_sizes,alpha))# 256/s==int
+            else:
+                m.append(ResBlock(feature_sizes, feature_sizes, alpha))
+
+            #if i == 10:
+            #    m.append(ConAttn(input_channels=feature_sizes, output_channels=feature_sizes, ksize=1, stride=1))
+        m.append(nn.Conv2d(feature_sizes,out_channels,kernel_size=3, bias=True,stride=1,padding=1))
+        self.net = nn.Sequential(*m)
+        self.gpu_ids=gpu_ids
+        self.net = init_net(self.net,"kaiming-uniform", self.gpu_ids)
+    
+    def forward(self, x):
+        #print(x.shape)
+        return x+self.net(x)
+    
---
+++ modules/model/baseline/ca.py
@@ -0,0 +1,130 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on May 9 10:24:49 2024
+
+@author: Wenli Huang
+"""
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class ConAttn(nn.Module):
+    def __init__(self, input_channels=128, output_channels=64, ksize=1, stride=1, rate=1, softmax_scale=1.):
+        super().__init__()
+        self.ksize = ksize
+        self.stride = stride
+        self.softmax_scale = softmax_scale
+        self.linear_weight = nn.Sequential(
+            nn.Conv2d(in_channels=input_channels // rate, out_channels=input_channels // (4*rate), kernel_size=ksize, stride=1,
+                      padding=ksize // 2),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(input_channels // (4*rate), out_channels=1, kernel_size=ksize, stride=1,
+                      padding=ksize // 2)
+        )
+        self.bias = nn.Sequential(
+            nn.Conv2d(in_channels=input_channels // rate, out_channels=input_channels // (4*rate), kernel_size=ksize, stride=1,
+                      padding=ksize // 2),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(input_channels // (4*rate), out_channels=1, kernel_size=ksize, stride=1,
+                      padding=ksize // 2)
+        )
+        self.query = nn.Conv2d(in_channels=input_channels, out_channels=input_channels//rate, kernel_size=1, stride=1, padding=0)
+        self.value = nn.Conv2d(in_channels=input_channels, out_channels=input_channels, kernel_size=1, stride=1, padding=0)
+        self.linear = nn.Sequential(
+            nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2, True)
+        )
+
+    def forward(self, x):
+        """ Attention
+            x1: value
+            x2: query
+            mask: Input mask, 0 indicates holes.
+            ksize: Kernel size for contextual attention.
+            stride: Stride for extracting patches from b.
+            rate: Dilation for matching.
+            softmax_scale: Scaled softmax for attention.
+        Returns:
+            torch.Tensor, reconstructed feature map.
+        """
+        # get shapes
+        x1 = self.value(x)
+        x2 = self.query(x)
+
+        x1s = list(x1.size())
+        x2s = list(x2.size())
+
+        # extract patches from low-level feature maps x1 with stride and rate
+        #kernel = 2 * self.rate
+        kernel = self.ksize
+        raw_w = extract_patches(x1, kernel=kernel, stride=self.stride)
+        raw_w = raw_w.contiguous().view(x1s[0], -1, x1s[1], kernel, kernel)  # B*HW*C*K*K
+
+        # split tensors by batch dimension; tuple is returned
+        raw_w_groups = torch.split(raw_w, 1, dim=0)
+
+        # split high-level feature maps x2 for matching
+        f_groups = torch.split(x2, 1, dim=0)
+        weight = self.linear_weight(x2)
+        bias = self.bias(x2)
+        weight_groups = torch.split(weight, 1, dim=0)
+        bias_groups = torch.split(bias, 1, dim=0)
+        # extract patches from x2 as weights of filter
+        w = extract_patches(x2, kernel=self.ksize, stride=self.stride)
+        w = w.contiguous().view(x2s[0], -1, x2s[1], self.ksize, self.ksize)  # B*HW*C*K*K
+
+        w_groups = torch.split(w, 1, dim=0)
+        # process mask
+        y = []
+        scale = self.softmax_scale
+        padding = 0 if self.ksize == 1 else (self.ksize - 1)//2
+        for xi, wi, raw_wi, wei, bii in zip(f_groups, w_groups, raw_w_groups, weight_groups, bias_groups):
+            '''
+            O => output channel as a conv filter
+            I => input channel as a conv filter
+            xi : separated tensor along batch dimension of front; 
+            wi : separated patch tensor along batch dimension of back; 
+            raw_wi : separated tensor along batch dimension of back; 
+            '''
+            # matching based on cosine-similarity
+            wi = wi[0]
+            escape_NaN = torch.FloatTensor([1e-4])
+            if torch.cuda.is_available():
+                escape_NaN = escape_NaN.cuda()
+            # normalize
+            wi_normed = wi / torch.max(torch.sqrt((wi * wi).sum([1, 2, 3], keepdim=True)), escape_NaN)
+            yi = F.conv2d(xi, wi_normed, stride=1, padding=padding)
+            yi = yi.contiguous().view(1, x2s[2] // self.stride * x2s[3] // self.stride, x2s[2], x2s[3]) # 1 * hw * h * w
+
+            sparse = F.relu(yi - yi.mean(dim=1, keepdim=True) * wei + bii)
+            sparse_r = (sparse != 0.).float()
+
+            # apply softmax to obtain
+            yi = yi * sparse
+            yi = F.softmax(yi * scale, dim=1)
+            yi = yi * sparse_r
+            yi = yi.clamp(min=1e-8)
+
+
+            # attending
+            wi_center = raw_wi[0]
+            yi = F.conv_transpose2d(yi, wi_center, stride=1, padding=padding) / (self.ksize * self.ksize)
+            y.append(yi)
+        y = torch.cat(y, dim=0)
+
+        y.contiguous().view(x1s)
+        y = self.linear(y)
+        y = y + x
+        return y
+
+
+# extract patches
+def extract_patches(x, kernel=3, stride=1):
+    if kernel != 1:
+        x = nn.ZeroPad2d((kernel-1) //2)(x)
+    x = x.permute(0, 2, 3, 1)
+    all_patches = x.unfold(1, kernel, stride).unfold(2, kernel, stride)
+    return all_patches
+
