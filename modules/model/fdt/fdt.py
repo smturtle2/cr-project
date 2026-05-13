b29ee201187680e999503febd0c8c9d05298e43c
dongju <smturtle2@kakao.com>
2026-05-14 00:25:15 +0900

    refactor: use independent common encoders

--- modules/model/fdt/fdt.py
+++ modules/model/fdt/fdt.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ..module.attention import MultiHeadAttention, TransformerLayer, _AmpRMSNorm
+from ..module.attention import TransformerLayer
 
 
 class _Residual3x3Block(nn.Module):
@@ -63,112 +63,6 @@ class ResizeConvUp(nn.Module):
         )
         x = self.residuals(x)
         return self.refine(x)
-
-
-class PairedCommonBlock(nn.Module):
-    def __init__(self, dim: int, heads: int = 4, mlp_ratio: int = 4):
-        super().__init__()
-        if dim % heads != 0:
-            raise ValueError("dim must be divisible by heads")
-        self.dim = dim
-        self.heads = heads
-        self.sar_query_norm = _AmpRMSNorm(dim)
-        self.sar_key_norm = _AmpRMSNorm(dim)
-        self.sar_value_norm = _AmpRMSNorm(dim)
-        self.cld_query_norm = _AmpRMSNorm(dim)
-        self.cld_key_norm = _AmpRMSNorm(dim)
-        self.cld_value_norm = _AmpRMSNorm(dim)
-        self.sar_cross_attn = MultiHeadAttention(dim, num_heads=heads)
-        self.cld_cross_attn = MultiHeadAttention(dim, num_heads=heads)
-
-        hidden_dim = dim * mlp_ratio
-        self.sar_mlp_norm = _AmpRMSNorm(dim)
-        self.cld_mlp_norm = _AmpRMSNorm(dim)
-        self.sar_mlp = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.ReLU(True),
-            nn.Linear(hidden_dim, dim),
-        )
-        self.cld_mlp = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.ReLU(True),
-            nn.Linear(hidden_dim, dim),
-        )
-
-    def forward(
-        self,
-        sar_state: torch.Tensor,
-        cld_state: torch.Tensor,
-        sar_anchor: torch.Tensor,
-        cld_anchor: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        sar_cross = sar_state + self.sar_cross_attn(
-            self.sar_query_norm(sar_state),
-            self.sar_key_norm(cld_anchor),
-            self.sar_value_norm(sar_anchor),
-        )
-        cld_cross = cld_state + self.cld_cross_attn(
-            self.cld_query_norm(cld_state),
-            self.cld_key_norm(sar_anchor),
-            self.cld_value_norm(cld_anchor),
-        )
-
-        sar_out = sar_cross + self.sar_mlp(self.sar_mlp_norm(sar_cross))
-        cld_out = cld_cross + self.cld_mlp(self.cld_mlp_norm(cld_cross))
-        return sar_out, cld_out
-
-
-class PairedCommonTransformer(nn.Module):
-    def __init__(self, dim: int, num_layers: int, heads: int):
-        super().__init__()
-        self.dim = dim
-        self.num_layers = num_layers
-        self.heads = heads
-        self.blocks = nn.ModuleList(
-            [PairedCommonBlock(dim, heads=heads) for _ in range(num_layers)]
-        )
-        self.sar_common_head = nn.Linear(dim, dim)
-        self.cld_common_head = nn.Linear(dim, dim)
-
-    @staticmethod
-    def _to_tokens(feature: torch.Tensor) -> torch.Tensor:
-        return feature.flatten(2).transpose(1, 2).contiguous()
-
-    @staticmethod
-    def _to_feature(
-        tokens: torch.Tensor,
-        height: int,
-        width: int,
-    ) -> torch.Tensor:
-        return (
-            tokens.transpose(1, 2)
-            .reshape(tokens.shape[0], tokens.shape[2], height, width)
-            .contiguous()
-        )
-
-    def forward(
-        self,
-        sar_feat: torch.Tensor,
-        cld_feat: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        height, width = sar_feat.shape[-2:]
-        sar_anchor = self._to_tokens(sar_feat)
-        cld_anchor = self._to_tokens(cld_feat)
-        sar_tokens = sar_anchor
-        cld_tokens = cld_anchor
-        for block in self.blocks:
-            sar_tokens, cld_tokens = block(
-                sar_tokens,
-                cld_tokens,
-                sar_anchor,
-                cld_anchor,
-            )
-        sar_common = self.sar_common_head(sar_tokens)
-        cld_common = self.cld_common_head(cld_tokens)
-        return (
-            self._to_feature(sar_common, height, width),
-            self._to_feature(cld_common, height, width),
-        )
 
 
 class Encoder(nn.Module):
@@ -233,6 +127,58 @@ class Encoder(nn.Module):
         return self._to_feature(tokens, height, width)
 
 
+class CommonEncoder(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        num_layers: int,
+        heads: int,
+    ):
+        super().__init__()
+        self.proj = nn.Sequential(
+            nn.Conv2d(
+                dim,
+                dim,
+                kernel_size=1,
+            ),
+            nn.GELU(),
+            nn.Conv2d(
+                dim,
+                dim,
+                kernel_size=3,
+                padding=1,
+                padding_mode="replicate",
+            ),
+        )
+        self.blocks = nn.ModuleList(
+            [TransformerLayer(dim, num_heads=heads) for _ in range(num_layers)]
+        )
+
+    @staticmethod
+    def _to_tokens(feature: torch.Tensor) -> torch.Tensor:
+        return feature.flatten(2).transpose(1, 2).contiguous()
+
+    @staticmethod
+    def _to_feature(
+        tokens: torch.Tensor,
+        height: int,
+        width: int,
+    ) -> torch.Tensor:
+        return (
+            tokens.transpose(1, 2)
+            .reshape(tokens.shape[0], tokens.shape[2], height, width)
+            .contiguous()
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        feature = self.proj(x)
+        height, width = feature.shape[-2:]
+        tokens = self._to_tokens(feature)
+        for block in self.blocks:
+            tokens = block(tokens)
+        return self._to_feature(tokens, height, width)
+
+
 # Feature Decomposition Transformer
 class FDT(nn.Module):
     def __init__(
@@ -263,11 +209,8 @@ class FDT(nn.Module):
         self.sar_encoder = Encoder(sar_channels, dim, num_layers, num_heads)
         self.cld_encoder = Encoder(cloudy_channels, dim, num_layers, num_heads)
 
-        self.common_extractor = PairedCommonTransformer(
-            dim,
-            num_layers=num_layers,
-            heads=num_heads,
-        )
+        self.sar_common_encoder = CommonEncoder(dim, num_layers, num_heads)
+        self.cld_common_encoder = CommonEncoder(dim, num_layers, num_heads)
         self.com_fuse = nn.Conv2d(self.up_dim * 2, self.common_dim, kernel_size=1)
         self.up = ResizeConvUp(dim)
 
@@ -275,7 +218,8 @@ class FDT(nn.Module):
         sar_feat_l = self.sar_encoder(sar)
         cld_feat_l = self.cld_encoder(cloudy)
 
-        sar_com_l, cld_com_l = self.common_extractor(sar_feat_l, cld_feat_l)
+        sar_com_l = self.sar_common_encoder(sar_feat_l)
+        cld_com_l = self.cld_common_encoder(cld_feat_l)
 
         sar_feat = self.up(sar_feat_l)
         cld_feat = self.up(cld_feat_l)
